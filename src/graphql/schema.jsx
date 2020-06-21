05c730854734634543019b27802014e128de3ad1
karl96k <karl.gustav1789@gmail.com>
2020-06-21 21:47:43 +0300

    add 1 new query

--- src/graphql/schema.jsx
+++ src/graphql/schema.jsx
@@ -1,47 +1,9 @@
-const MasterType = require('./entity/Master.jsx');
+let { GraphQLSchema } = require('graphql');
 
-const _ = require('lodash');
-const path = require("path");
-let { GraphQLList, GraphQLObjectType, GraphQLSchema } = require('graphql');
-
-const fs = require('fs');
-
-function getALLData(filename, label) {
-  let absolutePath = path.resolve(filename);
-  let rawdata = fs.readFileSync(absolutePath);
-  let parsed = JSON.parse(rawdata)['data'];
-
-  let return_data = [];
-
-  for (let i = 0; i < parsed.length; i++) {
-    if (parsed[i]['labels'][0] === label) {
-      const properties = parsed[i]['properties'];
-      return_data.push(properties);
-    }
-  }
-  return return_data;
-}
-
-const rawData = getALLData('./src/graphql/data/master.json', 'Master');
-
-
-
-const BlogQueryRootType = new GraphQLObjectType({
-  name: 'GdeltAppSchema',
-  description: 'GDELT Application Schema Query Root',
-  fields: () => ({
-    everything: {
-      type: new GraphQLList(MasterType),
-      description: 'This allows you do get an answer from every ROW',
-      resolve: function () {
-        return rawData;
-      },
-    }
-  }),
-});
+const root = require('./root.jsx');
 
 const GdeltAppSchema = new GraphQLSchema({
-  query: BlogQueryRootType,
+  query: root,
 });
 
 module.exports = GdeltAppSchema;
--- src/server.jsx
+++ src/server.jsx
@@ -5,14 +5,12 @@ const schema = require('./graphql/schema.jsx');
 let port = 3000;
 const app = express();
 
-
-
 app.use(
   '/',
   graphqlHTTP({
     schema: schema,
     graphiql: true,
-    pretty:true,
+    pretty: true,
   })
 );
 
