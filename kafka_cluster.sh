7390db19e1700d61c9436afe8d6d2ea93abc22bc
karl96k <karl.gustav1789@gmail.com>
2020-06-25 16:05:50 +0300

    Update kafka_cluster.sh

--- kafka_cluster.sh
+++ kafka_cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Deleting old files"
-rm -R zk-multiple-kafka-multiple
+rm -R zk-single-kafka-multiple
 rm -R mongodb
 echo "Building the Kafka cluster and MongoDB"
 docker-compose build "$@"--parallel
