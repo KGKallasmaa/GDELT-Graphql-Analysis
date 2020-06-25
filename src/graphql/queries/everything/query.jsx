80564d2af1c13e826c046f1eede81b1396bc06b1
karl96k <karl.gustav1789@gmail.com>
2020-06-25 14:51:48 +0300

    Add two example queries

--- src/graphql/queries/everything/query.jsx
+++ src/graphql/queries/everything/query.jsx
@@ -30,4 +30,43 @@ module.exports = {
       throw error;
     }
   },
+  get_actions_month: async ({ month }) => {
+    try {
+      const res = await Master.find({ MonthYear: month });
+      if (!res) {
+        return new Error('No results found.');
+      }
+      return res.map((single_res) => {
+        return transformMaster(single_res);
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+  get_results_between_time_periods: async ({ FractionDate_start, FractionDate_end }) => {
+    try {
+      const res = await Master.find({ FractionDate: { $gte: FractionDate_start, $lte: FractionDate_end } });
+      if (!res) {
+        return new Error('No results found.');
+      }
+      return res.map((single_res) => {
+        return transformMaster(single_res);
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+  get_results_between_tones: async ({ min_tone, max_tone }) => {
+    try {
+      const res = await Master.find({ AvgTone: { $gte: min_tone, $lte: max_tone } });
+      if (!res) {
+        return new Error('No results found.');
+      }
+      return res.map((single_res) => {
+        return transformMaster(single_res);
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
 };
