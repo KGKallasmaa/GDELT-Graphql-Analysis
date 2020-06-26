ddbb01b4fd706b0ed1985f2ae7ac99507105ec95
karl96k <karl.gustav1789@gmail.com>
2020-06-26 18:17:32 +0300

    Update shell scripts

--- start.sh
+++ start.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-echo "Installing Python dependencies"
-pip install -r requirements.txt
-echo "Starting to download .CSV files"
-cd src/graphql/data/ || exit
-python convert.py
-rm *.zip
-rm *.CSV
-cd ..
-cd ..
 echo "Installing Node dependencies"
 npm install
 echo "Start the server"
