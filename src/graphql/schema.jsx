de811cba4b0099e7c4b043bb97d4b0971d759531
karl96k <karl.gustav1789@gmail.com>
2020-06-27 18:12:48 +0300

    Add new queries to the schema

--- src/graphql/schema.jsx
+++ src/graphql/schema.jsx
@@ -36,36 +36,40 @@ type Master {
     EventRootCode:String
     QuadClass:String
     GoldsteinScale: String
-    NumMentions: String
-    NumSources: String
-    NumArticles: String
-    AvgTone: String
+    NumMentions: Int
+    NumSources: Int
+    NumArticles: Int
+    AvgTone: Float
     Actor1Geo_Type: String
     Actor1Geo_FullName: String
     Actor1Geo_CountryCode: String
     Actor1Geo_ADM1Code: String
     Actor1Geo_ADM2Code: String
-    Actor1Geo_Lat: String
-    Actor1Geo_Long: String
+    Actor1Geo_Lat: Float
+    Actor1Geo_Long: Float
     Actor1Geo_FeatureID: String
     Actor2Geo_Type: String
     Actor2Geo_FullName: String
     Actor2Geo_CountryCode: String
     Actor2Geo_ADM1Code:String
     Actor2Geo_ADM2Code:String
-    Actor2Geo_Lat: String
-    Actor2Geo_Long: String
+    Actor2Geo_Lat: Float
+    Actor2Geo_Long: Float
     Actor2Geo_FeatureID:String
     ActionGeo_Type: String
     ActionGeo_FullName: String
     ActionGeo_CountryCode:String
     ActionGeo_ADM1Code: String
     ActionGeo_ADM2Code: String
-    ActionGeo_Lat: String
-    ActionGeo_Long: String
+    ActionGeo_Lat: Float
+    ActionGeo_Long: Float
     ActionGeo_FeatureID: String
     DATEADDED: String
     SOURCEURL: String
+}
+type Top{
+  SQLDATE:String!,
+  events: [Master!]
 }
 
 """
@@ -77,6 +81,14 @@ type RootQuery {
     get_results_between_time_periods(FractionDate_start:Float,FractionDate_end:Float):[Master!]
     get_results_between_tones(min_tone:Float,max_tone:Float):[Master!]
     get_actions_month(month:String):[Master!]
+    
+    get_top_n_actors_with_most_mentions_per_day(n:Int,start_SQLDATE:String,end_SQLDATE:String):[Top!]
+    get_top_n_negative_actors_near_location(n:Int,
+                                            actor1Geo_Lat:Float,actor1Geo_Long:Float,
+                                            start_SQLDATE:String,end_SQLDATE:String):[Top!]
+                                            
+                                            
+    find_n_most_powerful_actor_events_using_pagerank_between_two_dates(n:Int,start_SQLDATE:String,end_SQLDATE:String):[Master!]                                      
 }
 
 
