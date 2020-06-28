2349793d687ce6c691c48b1b30b3d86d084cd43d
MaximSantalov <53902963+MaximSantalov@users.noreply.github.com>
2020-06-28 15:38:47 +0300

    Update simple_query.jsx

--- src/graphql/queries/simple_query.jsx
+++ src/graphql/queries/simple_query.jsx
@@ -44,6 +44,66 @@ module.exports = {
       throw error;
     }
   },
+  
+  get_actions_actor: async ({ actor }) => {
+    try {
+      const res = await Master.find({ Actor1Name: actor });
+      if (!res) {
+        return new Error('No results found.');
+      }
+      return res.map((single_res) => {
+        return transformMaster(single_res);
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+  
+  
+  get_actions_place: async ({ place }) => {
+    try {
+      const res = await Master.find({ ActionGeo_Fullname: place });
+      if (!res) {
+        return new Error('No results found.');
+      }
+      return res.map((single_res) => {
+        return transformMaster(single_res);
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+  
+  
+  top_positive: async ({ tone, n  }) => {
+    try {
+      const res = await Master.find({ AvgTone: { $gte : tone} }).sort({AvgTone : -1}).limit(n);
+      if (!res) {
+        return new Error('No results found.');
+      }
+      return res.map((single_res) => {
+        return transformMaster(single_res);
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+  
+   get_event_by_code: async ({ code, n }) => {
+    try {
+      const res = await Master.find({ EventCode: code }).limit(n);
+      if (!res) {
+        return new Error('No results found.');
+      }
+      return res.map((single_res) => {
+        return transformMaster(single_res);
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+  
+  
   get_results_between_time_periods: async ({ FractionDate_start, FractionDate_end }) => {
     try {
       const res = await Master.find({ FractionDate: { $gte: FractionDate_start, $lte: FractionDate_end } });
